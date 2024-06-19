5dc82d3b5bca8f03f501371e42662bec8fce39b0
Ian Andwati <andwatiian@gmail.com>
2024-06-19 03:19:08 +0200

    Added error handling

--- server.go
+++ server.go
@@ -2,16 +2,21 @@ package main
 
 import "github.com/gofiber/fiber/v2"
 
-
-func main(){
+func main() {
 	app := fiber.New()
 
 	app.Get("/", hello)
 
-	app.Listen(":3000")
+	err := app.Listen(":3000")
+	if err != nil {
+		return
+	}
 }
 
 func hello(c *fiber.Ctx) error {
-	c.SendString("Hello, World!")	
+	err := c.SendString("Hello, World!")
+	if err != nil {
+		return err
+	}
 	return nil
 }
